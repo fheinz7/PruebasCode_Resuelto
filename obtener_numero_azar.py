142a4e7699247663b8d42d4d25a7a4cceb302828
Felix Heinz <f.heinz7@gmail.com>
2019-06-11 17:03:28 -0400

    Pequeña simplificación del código

--- obtener_numero_azar.py
+++ obtener_numero_azar.py
@@ -1,5 +1,3 @@
-# import date
-import datetime
 import mechanicalsoup
 import threading
 
@@ -9,17 +7,16 @@ n_execution = 6                                                                 
 i = 0                                                                                           # Contador de consultas
 browser = mechanicalsoup.StatefulBrowser()                                                      # Inicialización del navegador web
 
-def print_message():
+def print_message():                                                                            # Tarea principal
     global i
 
     try:
         response = browser.open(dice_URL)                                                       # Abrir sitio web
         if response.status_code==200:                                                           # Verificar la respuesta del servidor
-            response = browser.get_current_page().find_all('h2', {'id' : 'result'})             # Buscar el elemento 'h2' con la propiedad id='result'
-            out_str = "Me salió un '" + response[0].next + "' el día "                          # Formar string con número del dado obtenido
-            response = browser.get_current_page().find_all('p', {'id' : 'time'})                # Buscar el elemento 'p' con la propiedad id='time'
-            out_str = out_str + response[0].next                                                # Formar string final
-            print(out_str)                                                                      # Mostrar string por el stdout
+            print("Me salió un '" + \
+                browser.get_current_page().find_all('h2', {'id' : 'result'})[0].next + \
+                "' el día " + \
+                browser.get_current_page().find_all('p', {'id' : 'time'})[0].next)              # Imprimir salida del programa
         else:
             print("Error. Código de respuesta:", response.status_code)                          # Imprimir código de respuesta en caso de error
     except:
@@ -30,5 +27,6 @@ def print_message():
         return
     else:
         threading.Timer(timeout, print_message).start()                                         # Crear un timer para la ejecución de la tarea en un hilo distinto, para no colgar el programa
+
 
 print_message()                                                                                 # Comienzo de ejecución de la tarea
